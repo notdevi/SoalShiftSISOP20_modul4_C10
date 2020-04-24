d4dcde5b15ad5f73d8cd0157bf3ee1949ae74285
anggarayp <61231385+anggarayp@users.noreply.github.com>
2020-04-24 19:14:23 +0700

    Update ssfs.c

--- ssfs.c
+++ ssfs.c
@@ -134,42 +134,9 @@ void decrypt_v1(char *path) {
 	}			 			 	
 }
 
-// encrypt yang di comment
-/* 
-void encrypt_c(char str[]) {
-	char cur;
-	
-	for(int i=0; i<strlen(str); i++) {
-		cur = str[i];
-		for(int j=0; j<caesar+key; j++) {
-			if(cur == caesar_key[j]) {
-				cur = caesar_key[j+key];
-				str[i] = cur;
-				break;
-			}
-		}
-	}
-}
-
-void decrypt_c(char str[]) {
-	char cur;
-
-	for(int i=(strlen(str))-1; i>=0; i--) {
-		cur = str[i];
-		for(int j=caesar+key-1; j>=0; j--) {
-			if(cur == caesar_key[j]) {
-				cur = caesar_key[j-key];
-				str[i] = cur;
-				break;
-			}
-		}
-	}
-}
-*/
-
 char *LOG = "/home/devi/fs.log";
 
-void warning(char* desc) {
+void log_warning(char* desc) {
 	FILE *file_log = fopen(LOG, "a");
 
 	time_t rawtime = time(NULL);
@@ -187,7 +154,7 @@ void warning(char* desc) {
 	fclose(file_log);
 }
 
-void info(char* desc) {
+void log_info(char* desc) {
 	FILE *file_log = fopen(LOG, "a");
 
 	time_t rawtime = time(NULL);
@@ -205,12 +172,186 @@ void info(char* desc) {
 	fclose(file_log);
 }
 
+//ngebaca file
+static int xmp_getattr(const char *path, struct stat *stbuf) {
+    	log_info("LS");
+	int res;
+
+	res = lstat(path, stbuf);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+		       off_t offset, struct fuse_file_info *fi) {
+    	log_info("CD");
+	DIR *dp;
+	struct dirent *de;
+
+	(void) offset;
+	(void) fi;
+
+	dp = opendir(path);
+	if (dp == NULL)
+		return -errno;
+
+	while ((de = readdir(dp)) != NULL) {
+		struct stat st;
+		memset(&st, 0, sizeof(st));
+		st.st_ino = de->d_ino;
+		st.st_mode = de->d_type << 12;
+		if (filler(buf, de->d_name, &st, 0))
+			break;
+	}
+
+	closedir(dp);
+	return 0;
+}
+
+static int xmp_read(const char *path, char *buf, size_t size, off_t offset, struct fuse_file_info *fi) {
+    	log_info("CAT");
+	int fd;
+	int res;
+
+	(void) fi;
+	fd = open(path, O_RDONLY);
+	if (fd == -1)
+		return -errno;
+
+	res = pread(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+
+	close(fd);
+	return res;
+}
+
+static int xmp_mkdir(const char *path, mode_t mode) {
+   	log_info("MKDIR");
+	int res;
+
+	res = mkdir(path, mode);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_mknod(const char *path, mode_t mode, dev_t rdev) {
+    	log_info("CREATE");
+	int res;
+
+	/* On Linux this could just be 'mknod(path, mode, rdev)' but this
+	   is more portable */
+	if (S_ISREG(mode)) {
+		res = open(path, O_CREAT | O_EXCL | O_WRONLY, mode);
+		if (res >= 0)
+			res = close(res);
+	} else if (S_ISFIFO(mode))
+		res = mkfifo(path, mode);
+	else
+		res = mknod(path, mode, rdev);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+//delete data
+static int xmp_unlink(const char *path) {
+    	log_warning("REMOVE");
+	int res;
+
+	res = unlink(path);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+//delete direktori
+static int xmp_rmdir(const char *path) {
+    log_warning("RMDIR");
+	int res;
+
+	res = rmdir(path);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_rename(const char *from, const char *to) {
+    log_info("MOVE");
+	int res;
+
+	res = rename(from, to);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_truncate(const char *path, off_t size) {
+    log_info("TRUNCATE");
+	int res;
+
+	res = truncate(path, size);
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_open(const char *path, struct fuse_file_info *fi) {
+    log_info("OPEN");
+	int res;
+
+	res = open(path, fi->flags);
+	if (res == -1)
+		return -errno;
+
+	close(res);
+	return 0;
+}
+
+static int xmp_write(const char *path, const char *buf, size_t size,
+		     off_t offset, struct fuse_file_info *fi) {
+    	log_info("WRITE");
+	int fd;
+	int res;
+
+	(void) fi;
+	fd = open(path, O_WRONLY);
+	if (fd == -1)
+		return -errno;
+
+	res = pwrite(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+
+	close(fd);
+	return res;
+}
+
+static struct fuse_operations xmp_oper = {
+	.getattr = xmp_getattr,
+	.readdir = xmp_readdir,
+	.read = xmp_read,
+	.mkdir = xmp_mkdir,
+	.mknod = xmp_mknod,
+	.unlink = xmp_unlink,
+	.rmdir = xmp_rmdir,
+	.rename = xmp_rename,
+	.truncate = xmp_truncate,
+	.open = xmp_open,
+	.read = xmp_read,
+	.write = xmp_write,
+};
+
 int main(int argc, char *argv[]) {
-	char test[] = "encv1_rahasia/FOTO_PENTING/kelincilucu.jpg";
-	
-	generate_codomain(10);
-	encrypt_v1(test);
-	puts(test);
-	decrypt_v1(test);
-	puts(test);
+	umask(0);
+	return fuse_main(argc, argv, &xmp_oper, NULL);
 }
